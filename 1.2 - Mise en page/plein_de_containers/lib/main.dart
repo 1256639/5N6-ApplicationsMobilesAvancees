7cee5ceb79fc97d89e4576d7040a3268e97a4910
Patrick Rolleston-Chase <1256639@cegepmontpetit.ca>
2025-08-28 16:37:50 -0400

    FCT Plein de containers

--- 1.2 - Mise en page/plein_de_containers/lib/main.dart
+++ 1.2 - Mise en page/plein_de_containers/lib/main.dart
@@ -31,7 +31,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-
+    //Comment
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
